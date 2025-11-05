c9f3bbb9e7ac8150719a0d56f5464ab0fe3aedb9
a97424mc <mohammad.chaudhry-2@student.manchester.ac.uk>
2025-11-05 16:46:17 +0000

    feat:transform df to match db

--- etl/extract.py
+++ etl/extract.py
@@ -1,9 +1,12 @@
 import yfinance as yf
 import pandas as pd
 
+#need to get name, symbol, ohlc, volume to fill the db name
 def fetch_data(ticker: str, start: str, end: str): 
-    df = yf.download([ticker], start = start, end = end)
-    return df
+    symbol = yf.Ticker(ticker)
+    name = symbol.get_info()
+    ohlcv_df = yf.download([symbol], start = start, end = end)
+    return symbol, name, ohlcv_df
 
 
 
---
+++ etl/transform.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import yfinance as yf
+
+#need to get name, symbol, ohlc, volume to fill the db name
+ohlcv_df = yf.download("MSFT", start = "2025-10-11", end = "2025-10-16")
+def clean_data(name: str, symbol: str, dataframe:pd.DataFrame):
+    dataframe = dataframe.reset_index()
+    dataframe = dataframe.rename(columns={
+        "Open":"open", 
+        "High":"high", 
+        "Low":"low", 
+        "Close":"close", 
+        "Volume":"volume", 
+        "Date":"date"})
+    dataframe['ticker'] = symbol
+    dataframe = dataframe.dropna()
+    print(dataframe.head())
+    
+    
+clean_data('Microsoft', "MSFT", ohlcv_df)
